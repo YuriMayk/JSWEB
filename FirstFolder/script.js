37ff4657403d5717fff20a9fb8a0c78751b90eb2
Yuri Mayk <yuri.mayk@gmail.com>
2022-01-24 21:43:10 -0300

    Second commit, created FirstFolder and a HTMLfile named index.html and a Javascript file named script.js.

---
+++ FirstFolder/script.js
@@ -0,0 +1,3 @@
+const element = document.getElementById("main-text")
+const elements = document.getElementsByClassName("paragraph")
+console.log(elements)
---
+++ README.md
@@ -0,0 +1,1 @@
+## Javascript's Exercices - DOM
