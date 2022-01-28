b50a01936503893b0fbc2b72a7a922471db8a2e7
Yuri Mayk <yuri.mayk@gmail.com>
2022-01-27 21:32:36 -0300

    Expanded the Selecors, added information about how change an html information, and how to change style of a tag.

--- FirstFolder/script.js
+++ FirstFolder/script.js
@@ -1,4 +1,34 @@
 const element = document.getElementById("main-text")
 const elements = document.getElementsByClassName("paragraph")
 const elementsTag = document.getElementsByTagName("h1")
-console.log(elementsTag)
+const elementsName = document.getElementsByName("first-input")
+const elementquery = document.querySelector("p")
+const elementqueryall = document.querySelectorAll("p")
+
+const elementt = document.getElementById("main-text")
+
+// The sequence below, chamge the content of a tag.
+// innerText and textContent change the caracters inside tag, without format.
+//innerHTML accept Tag's.
+//elementt.innerText = "Changed Text!"
+//elementt.textContent = "Changed Text!"
+//elementt.innerHTML = "Changed Text! <sup>Tag</sup>"
+
+elementt.style.backgroundColor = '#ffee00'
+
+console.log(elementqueryall)
+console.log(element.attributes)
+
+
+/*
+The selector getElementById return that one first element.
+
+The following selectors return a 'HTML COLECTION':
+- getElementsByClassName
+- getElementByTagName
+
+The following selectors return a 'NodeList':
+- getElementsByName
+- querySelector
+- querySelectorAll
+  */
